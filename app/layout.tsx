88bdbb17b2dbdf0a8c072c57d63aa2164e62c0c2
Cherifi, mohamed iyad <mc02589@surrey.ac.uk>
2026-02-06 22:22:26 +0000

    chore: production readiness fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,6 @@ import CustomCursor from '@/components/CustomCursor';
 import Preloader from '../components/Preloader';
 import StickyEmail from './_components/StickyEmail';
 import { GoogleAnalytics } from '@next/third-parties/google';
-import Script from 'next/script';
 
 const antonFont = Anton({
     weight: '400',
@@ -39,6 +38,8 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+    const gaId = process.env.NEXT_PUBLIC_GA_ID;
+
     return (
         <html lang="en">
             <body
@@ -61,6 +62,8 @@ export default function RootLayout({
                     <ParticleBackground />
                     <StickyEmail />
                 </ReactLenis>
+
+                {gaId ? <GoogleAnalytics gaId={gaId} /> : null}
             </body>
         </html>
     );
